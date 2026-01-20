35817f545ed93c16dbeda47109d0921d9b3edd91
mjkard <mammanseydi@gmail.com>
2026-01-20 18:22:50 +0100

    Update variables_MGK.py

--- 02_Day_Variables_builtin_functions/day_2/variables_MGK.py
+++ 02_Day_Variables_builtin_functions/day_2/variables_MGK.py
@@ -1,4 +1,5 @@
 # Day 2: 30 days of Python Programming
+# Exercise - 1
 first_name = 'Muhammad'
 last_name = 'Gulma'
 full_name = 'Muhammad Gulma'
@@ -10,3 +11,5 @@ is_married = False
 is_true = True
 is_light_on = True
 education = {'primary school' : 'Vital years', 'Sec_school': 'Zaria Academy', 'College' : 'Ruhr University'}
+#Exercise -2
+print(education)
