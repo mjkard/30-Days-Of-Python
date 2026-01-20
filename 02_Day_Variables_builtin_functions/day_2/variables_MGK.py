08366655e6a403c7a9a3f87cb8b77a334eaee5cc
mjkard <mammanseydi@gmail.com>
2026-01-20 18:19:56 +0100

    Update variables_MGK.py

--- 02_Day_Variables_builtin_functions/day_2/variables_MGK.py
+++ 02_Day_Variables_builtin_functions/day_2/variables_MGK.py
@@ -1,1 +1,12 @@
-
+# Day 2: 30 days of Python Programming
+first_name = 'Muhammad'
+last_name = 'Gulma'
+full_name = 'Muhammad Gulma'
+country = 'Nigeria'
+city = 'Kebbi'
+age = 26
+year = 1999
+is_married = False
+is_true = True
+is_light_on = True
+education = {'primary school' : 'Vital years', 'Sec_school': 'Zaria Academy', 'College' : 'Ruhr University'}
