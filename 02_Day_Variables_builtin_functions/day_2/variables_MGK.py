967627d8db86f90fdd3b130628d5f4b1094af17b
mjkard <mammanseydi@gmail.com>
2026-01-20 19:04:47 +0100

    Update variables_MGK.py

--- 02_Day_Variables_builtin_functions/day_2/variables_MGK.py
+++ 02_Day_Variables_builtin_functions/day_2/variables_MGK.py
@@ -13,3 +13,59 @@ is_light_on = True
 education = {'primary school' : 'Vital years', 'Sec_school': 'Zaria Academy', 'College' : 'Ruhr University'}
 #Exercise -2
 print(education)
+>>> #Exercise -2
+>>> # Proceeding to check all date types of the above
+>>> print(type(last_name))
+<class 'str'>
+>>> print(type(first_name))
+<class 'str'>
+>>> print(type(full_name))
+<class 'str'>
+>>> print(type(country))
+<class 'str'>
+>>> print(type(age))
+<class 'int'>
+>>> print(type(year))
+<class 'int'>
+>>> print(type(is_married))
+<class 'bool'>
+>>> print(type(is_true))
+<class 'bool'>
+>>> print(type(is_light_on))
+<class 'bool'>
+>>> print(type(education))
+<class 'dict'>
+>>> print(len(first_name))
+8
+>>> print(len(last_name))
+5
+print(len(last_name)>len(first_name))
+False
+>>> print(num_two, num_one)
+4 5
+>>> reminder = num_one % num_two
+>>> print('reminder: ', reminder)
+#exercise 2 task 12
+>>> circle_radius = 30
+>>> area_circle = 3.14 * circle_radius**2
+>>> print('area of the circle is: ', area_circle)
+area of the circle is:  2826.0
+>>> circum_of_circle = 2*3.14*circule_radius
+circum_of_circle = 2*3.14*circle_radius
+>>> print("circumference is: ", circum_of_circle)
+circumference is:  188.4
+>>> radius = input("What's the radius: ")
+What's the radius: 20
+>>> area= 3.14 * radius **2
+radius=float(radius)
+>>> area= 3.14 * radius **2
+>>> print('your circle area is: ', area)
+your circle area is:  1256.0
+#Collecting user data
+first_name = input("Type your first name please: ")
+last_name = input("Type your last name please: ")
+country = input("Country of origin: ")
+age = input('Your age please: ')
+print('your info is: ', first_name,last_name,country, age ,  "Thank you for using our services!"   )
+#code executed successfully!!
+#Day two of Python completed
